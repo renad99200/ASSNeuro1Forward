387a3524ef88c57d8caaf24e1354fe8e6d3a316c
renad99200 <rynadmad14@gmail.com>
2026-05-14 11:27:01 -0700

    Modify target values and print total error
    Updated target values and added total error print statement.

--- REassignmentNeuro1.py
+++ REassignmentNeuro1.py
@@ -3,7 +3,6 @@ import math
 i1 = 0.05
 i2 = 0.10
 
-
 w1 = 0.15
 w2 = 0.20
 w3 = 0.25
@@ -13,14 +12,11 @@ w6 = 0.45
 w7 = 0.50
 w8 = 0.55
 
-
 b1 = 0.5
 b2 = 0.7
 
-
-t1 = 0.1
+t1 = 0.01
 t2 = 0.99
-
 
 net_h1 = (w1 * i1) + (w2 * i2) + b1
 net_h2 = (w3 * i1) + (w4 * i2) + b1
@@ -28,20 +24,19 @@ net_h2 = (w3 * i1) + (w4 * i2) + b1
 out_h1 = math.tanh(net_h1)
 out_h2 = math.tanh(net_h2)
 
-
+# output layer
 net_o1 = (w5 * out_h1) + (w6 * out_h2) + b2
 net_o2 = (w7 * out_h1) + (w8 * out_h2) + b2
 
 o1 = math.tanh(net_o1)
 o2 = math.tanh(net_o2)
 
-
 error1 = 0.5 * ((t1 - o1) ** 2)
 error2 = 0.5 * ((t2 - o2) ** 2)
 
 total_error = error1 + error2
 
-
 print("o1 =", o1)
 print("o2 =", o2)
+
 print("Total Error =", total_error)
